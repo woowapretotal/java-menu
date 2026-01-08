9308ca584cf98d036887d1ae33e2d03affa6dc63
Junseo Bae <ferrater1013@gmail.com>
2026-01-08 21:09:03 +0900

    fix: 누락된 프롬프트 추가

--- src/main/java/menu/application/view/ConsoleOutputView.java
+++ src/main/java/menu/application/view/ConsoleOutputView.java
@@ -17,6 +17,7 @@ public class ConsoleOutputView {
     }
 
     public void printDrawResult(DrawResponse drawResponse) {
+        System.out.println("메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
         System.out.println(OutputFormatter.formatCategory(drawResponse.categoryNames()));
         System.out.print(OutputFormatter.formatCoachDrawResults(drawResponse.coachDrawResponses()) + GlobalMessage.BLANK_AND_NEW_LINE.get());
